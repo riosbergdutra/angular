7b16a26a52b221da615489497a4622515b2bda6d
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-05-11 05:44:18 -0300

    Compartilhamento de Dados

--- curso-angular/src/app/app.module.ts
+++ curso-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentDateComponent } from './components/parent-date/parent-date.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponentComponent
+    FirstComponentComponent,
+    ParentDateComponent
   ],
   imports: [
     BrowserModule
