4a2c6353feaff48b3994780003fb7005add91421
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-05-21 13:25:44 -0300

    importância das interfaces

---
+++ curso-angular/src/app/animal.ts
@@ -0,0 +1,8 @@
+ export interface Animal {
+    name: string
+    type: string
+    age: number
+}
+
+// export serve para exportar e import importar 
+// interface é um conjunto de regras que deve ser seguida a risca
--- curso-angular/src/app/components/list-render/list-render.component.ts
+++ curso-angular/src/app/components/list-render/list-render.component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
 
+import { Animal } from 'src/app/animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
-  animals = [
-    {name: "Turca", type: "Dog"},
-    {name: "Tom", type: 'Cat'},
-    {name: 'Frida', type: 'Dog'},   
-    {name: 'Bob', type: 'Horse'},
+  animals: Animal [] = [
+    {name: "Turca", type: "Dog", age: 4},
+    {name: "Tom", type: 'Cat', age: 10},
+    {name: 'Frida', type: 'Dog', age: 5},   
+    {name: 'Bob', type: 'Horse', age: 1},
+    // este [] é para ARRAYS
   ]
+
   eletronico = [
     'Telefone', 'celular', 'computador',
   ]
-  animal = {
+
+    animal: Animal =  {
     name: 'Teste',
-    type: 'Alguma coisa'
-  }
+    type: 'Alguma coisa',
+    age: 2
+  } // este animal: Animal significa que ele recebe o Animal assim como se eu colocasse object, number, string etc...
+
+  animalDetails = '' // serve para ser uma variavel para oque estiver escrito ali em baixo deverá ser exibida aqui
+
+  showage(animal: Animal) {
+  this.animalDetails =  `O pet ${animal.name} tem ${animal.age} anos`
+  } // o "animal.name" e o "animal.age" serve para dizer que me refiro a array name do animal
 }
