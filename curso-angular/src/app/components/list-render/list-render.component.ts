2de7b22ab2446bffa0a7daf858b594e088b532f8
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2023-05-24 18:40:57 -0300

    Services

--- curso-angular/src/app/components/list-render/list-render.component.ts
+++ curso-angular/src/app/components/list-render/list-render.component.ts
@@ -4,7 +4,7 @@ import { Animal } from 'src/app/animal';
 // interface
 
 import { ListService } from 'src/app/services/list.service';
-//este é o service chamado list-render
+//este é o service chamado list-service
 
 @Component({
   selector: 'app-list-render',
@@ -37,4 +37,17 @@ export class ListRenderComponent {
   } // o "animal.name" e o "animal.age" serve para dizer que me refiro a array name do animal
 
  constructor (private listService: ListService) {}
+ // coloque em privado
+ // precisa criar um constructor () {} para dar certo o services
+ // o angular vai entender que eu automaticamente que eu criei um novo serviço que vai se chamar listService e ele vem de ListService
+ // crie com um começo minusculo para diferenciar da classe da inportação
+ //O constructor é um método especial em uma classe que é usado para inicializar propriedades e executar lógica de inicialização. Ele é chamado automaticamente quando um objeto da classe é criado e é útil para definir valores padrão para as propriedades e realizar outras tarefas de configuração necessárias.
+
+
+ removeAnimal (animal: Animal){
+  console.log('removendo animal...')
+  this.animals = this.listService.remove(this.animals, animal);
+  //this.animals array completo;
+  //animal parametro "animal especifico";
+}
 }
--- curso-angular/src/app/services/list.service.ts
+++ curso-angular/src/app/services/list.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Animal } from '../animal';
+//importa o animal
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,11 @@ import { Injectable } from '@angular/core';
 export class ListService {
 
   constructor() { }
+  remove(animals: Animal[], animal: Animal) {
+    return animals.filter((a) => animal.name !== a.name) 
+  } // o !== é para diferenciar pq se n tiver o ! vai apagar todos os animais e n o animal especifico clicado
+
+  //"=>" é uma arrow function que serve como um critério de filtro para a função filter. Ela verifica se o nome do animal passado como argumento é diferente do nome de cada elemento a do array animals, e retorna true se a condição for verdadeira, indicando que o elemento deve ser mantido no novo array filtrado.
+
+  //A função filter é usada para criar um novo array com elementos que atendem a um critério específico. Ela filtra os elementos com base em uma função que determina se um elemento deve ser incluído ou não no novo array. É uma forma de selecionar apenas os elementos desejados de um array original.
 }
